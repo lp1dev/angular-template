73c9474f93521b6af006efd05148298c9f2b7564
Lupin <lupin@lp1.eu>
2016-10-28 19:56:09 +0200

    removed www directory

--- www/js/app.js
+++
@@ -1,4 +0,0 @@
-angular.module('app', ['ngRoute', 'ui.router']);
-appControllers = angular.module('app.controllers', [])
-appServices = angular.module('app.services', [])
-appDirectives = angular.module('app.directives', [])
--- www/js/app.modules.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', [
-    'app.services',
-    'app.directives',
-    'app.controllers',
-    'ngRoute'
-]);
-
-var appControllers = angular.module('app.controllers', []);
